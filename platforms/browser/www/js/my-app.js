6363cd26299fc04c0fa128f7dfb3dbaecd4bacb6
Jack Rivers <jackrivers.studio@gmail.com>
2018-02-24 16:39:39 +0100

    icon update

--- platforms/browser/www/js/my-app.js
+++ platforms/browser/www/js/my-app.js
@@ -55,13 +55,16 @@ var sounds = ['A','B','C','D','E','F','G','A&#9837;','B&#9837;','C&#9837;','D&#9
 var strings = [1,2,3,4,5,6];
 var positions = ['0-3','3-7','7-10','10-13'];
 var training = false;
+var t;
 
 function toggleTraining(e){
     training = !training;
     if(training){
         lottery();
+        t = setInterval(function(){ lottery() }, 6000);
         $$(e.target).html('Stop');
     } else {
+        clearInterval(t);
         $$(e.target).html('Start');
     }
 }
@@ -72,6 +75,7 @@ function stopTraining(){
 
 
 function lottery(){
+    console.log('lottery');
     if(training){
         var sound = sounds[Math.floor(Math.random()*sounds.length)];
         var string = strings[Math.floor(Math.random()*strings.length)];
@@ -79,7 +83,7 @@ function lottery(){
         $$('#sound').html(sound);
         $$('#string').html(string);
 
-        setTimeout("lottery()", 6000);
+//        setTimeout("lottery()", 6000);
     }
 
 }
